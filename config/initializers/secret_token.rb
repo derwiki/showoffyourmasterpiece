1227c56dd6cf712c2ad4b6645e4ba6d80d49ad6d
Adam Derewecki <derewecki@gmail.com>
2011-10-25 09:00:51 -0700

    rails new showoffyourmasterpiece

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showoffyourmasterpiece::Application.config.secret_token = '50b552537733a063bea4f4e0eaa6c838c91184547c9cdf286ecde272eb392f859c6840bda283721cd4c344dad531e24fa8a43f61b2c97acf321d2185e65284d2'
