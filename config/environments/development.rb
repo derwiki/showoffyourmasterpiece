daa836ddf5a2395eea50ac6f75a702f6d8830600
Adam Derewecki <derewecki@gmail.com>
2011-11-16 08:11:10 -0800

    Add personal GMail for dev sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Showoffyourmasterpiece::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -27,4 +24,16 @@ Showoffyourmasterpiece::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Use GMail for email delivery
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :tls            => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => 'derewecki@gmail.com',
+    :password       => 'Wr&-U2&frE'
+  }
 end
